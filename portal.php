989d025ae723e2cba71e85f5119e9336ac74a0a5
mcaupybugs <vishalydv.me@gmail.com>
2020-09-21 14:33:05 +0530

    Made portal page

--- portal.php
+++ portal.php
@@ -23,31 +23,33 @@ $result = mysqli_query($connection, $query);
 
 ?>
 
+<body style="background-color: teal;">
 
-<div style="margin-top:100px">
-    <table class="table table-dark">
-        <thead>
-            <tr>
-                <th scope="col">Book ID</th>
-                <th scope="col">Book Name</th>
-                <th scope="col">Address</th>
-                <th scope="col">User Owning</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            while ($row = mysqli_fetch_row($result)) {
-            ?>
+    <div class="container" style="margin-top:100px">
+        <table class="table table-dark">
+            <thead>
                 <tr>
-
-                    <th scope="row"><?php print_r($row[0]) ?></th>
-                    <td><?php print_r($row[1]) ?></td>
-                    <td><?php print_r($row[2]) ?></td>
-                    <td><?php print_r($row[3]) ?></td>
+                    <th scope="col">Book ID</th>
+                    <th scope="col">Book Name</th>
+                    <th scope="col">Address</th>
+                    <th scope="col">User Owning</th>
                 </tr>
-            <?php
-            }
-            ?>
-        </tbody>
-    </table>
-</div>
+            </thead>
+            <tbody>
+                <?php
+                while ($row = mysqli_fetch_row($result)) {
+                ?>
+                    <tr>
+
+                        <th scope="row"><?php print_r($row[0]) ?></th>
+                        <td><?php print_r($row[1]) ?></td>
+                        <td><?php print_r($row[2]) ?></td>
+                        <td><?php print_r($row[3]) ?></td>
+                    </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</body>
